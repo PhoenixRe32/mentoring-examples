8fba4983011d4cff1110b1ac657dd9627cd26461
Andreas Ανδρέου <andreas.andreou@sky.uk>
2021-05-27 15:29:28 +0100

    Move initialisation into constructor

--- person-reader-printer/src/main/java/com/pittacode/PersonBuilder.java
+++ person-reader-printer/src/main/java/com/pittacode/PersonBuilder.java
@@ -4,10 +4,17 @@ import java.io.IOException;
 
 public class PersonBuilder {
 
-    private final IntegerReader ageReader = new IntegerReader("age", 0, 150);
-    private final IntegerReader yearReader = new IntegerReader("year of birth");
-    private final IntegerReader heightReader = new IntegerReader("height", 30, 300);
-    private final StringReader nameReader = new StringReader("name");
+    private final IntegerReader ageReader;
+    private final IntegerReader yearReader;
+    private final IntegerReader heightReader;
+    private final StringReader nameReader;
+
+    public PersonBuilder() {
+        this.ageReader = new IntegerReader("age", 0, 150);
+        this.yearReader = new IntegerReader("year of birth");
+        this.heightReader = new IntegerReader("height", 30, 300);
+        this.nameReader = new StringReader("name");
+    }
 
     public Person build() throws IOException {
         String name = nameReader.read();
