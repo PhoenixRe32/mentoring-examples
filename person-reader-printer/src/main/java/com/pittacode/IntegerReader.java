b5bfab0086f4f5cc379a862313d4f2e288cc7672
Andreas Ανδρέου <andreas.andreou@sky.uk>
2021-05-27 15:20:29 +0100

    Allow number readers to enforce limits

--- person-reader-printer/src/main/java/com/pittacode/IntegerReader.java
+++ person-reader-printer/src/main/java/com/pittacode/IntegerReader.java
@@ -10,21 +10,36 @@ public class IntegerReader {
     private final Scanner scanner;
     private final BufferedReader reader;
     private final String label;
+    private final int min;
+    private final int max;
 
     public IntegerReader(String label) {
+        this(label, Integer.MIN_VALUE, Integer.MAX_VALUE);
+    }
+
+    public IntegerReader(String label, int min, int max) {
         this.label = label;
+        this.min = min;
+        this.max = max;
         this.scanner = new Scanner(System.in);
         this.reader = new BufferedReader(new InputStreamReader(System.in));
     }
 
     public int scan() {
         System.out.printf("Enter %s: ", label);
-        return scanner.nextInt();
+        return validate(scanner.nextInt());
     }
 
     public int read() throws IOException {
         System.out.printf("Enter %s: ", label);
         final String line = reader.readLine();
-        return Integer.parseInt(line);
+        return validate(Integer.parseInt(line));
+    }
+
+    private int validate(int i) {
+        if (min > i || i > max) {
+            throw new IllegalArgumentException(String.format("%s input should be between %d and %d", label, min, max));
+        }
+        return i;
     }
 }
--- person-reader-printer/src/main/java/com/pittacode/Main.java
+++ person-reader-printer/src/main/java/com/pittacode/Main.java
@@ -4,9 +4,9 @@ import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        final IntegerReader ageReader = new IntegerReader("age");
+        final IntegerReader ageReader = new IntegerReader("age", 0, 150);
         final IntegerReader yearReader = new IntegerReader("year of birth");
-        final IntegerReader heightReader = new IntegerReader("height");
+        final IntegerReader heightReader = new IntegerReader("height", 30, 300);
         final StringReader nameReader = new StringReader("name");
 
         String name = nameReader.read();
