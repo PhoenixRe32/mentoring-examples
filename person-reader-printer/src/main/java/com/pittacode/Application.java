232ba869a14b79176ed945a2f60e7cabca3a2160
Andreas Ανδρέου <andreas.andreou@sky.uk>
2021-05-27 15:33:37 +0100

    Print error message to console

--- person-reader-printer/src/main/java/com/pittacode/Application.java
+++ person-reader-printer/src/main/java/com/pittacode/Application.java
@@ -1,9 +1,8 @@
 package com.pittacode;
 
-import java.io.IOException;
-
 public class Application {
-    public static void main(String[] args) throws IOException {
+
+    public static void main(String[] args) {
         IntegerReader ageReader = new IntegerReader("age", 0, 150);
         IntegerReader yearReader = new IntegerReader("year of birth", 1900, 2021);
         IntegerReader heightReader = new IntegerReader("height", 30, 300);
@@ -11,7 +10,11 @@ public class Application {
 
         PersonBuilder personBuilder = new PersonBuilder(ageReader, yearReader, heightReader, nameReader);
 
-        Person person = personBuilder.build();
-        System.out.println(person.toString());
+        try {
+            Person person = personBuilder.build();
+            System.out.println(person.toString());
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
