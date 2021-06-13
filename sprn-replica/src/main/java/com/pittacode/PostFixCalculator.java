7c7f08843a9732db3cd0d892ca1e60d0cd2de2d2
Andreas Ανδρέου <andreas.andreou@sky.uk>
2021-06-13 22:06:34 +0300

    Postfix - add random number to stack

--- sprn-replica/src/main/java/com/pittacode/PostFixCalculator.java
+++ sprn-replica/src/main/java/com/pittacode/PostFixCalculator.java
@@ -6,6 +6,7 @@ import java.util.Collection;
 import java.util.Collections;
 import java.util.Deque;
 import java.util.HashSet;
+import java.util.List;
 
 public class PostFixCalculator {
 
@@ -27,15 +28,23 @@ public class PostFixCalculator {
     private final static Collection<String> ACTIONS = toSet(Arrays.asList(PRINT_LAST_INPUT, PRINT_STACK));
 
     private static final int MAX_STACK_SIZE = 23;
+    private static final List<Integer> RANDOMS = Arrays.asList(1804289383, 846930886, 1681692777, 1714636915,
+                                                               1957747793, 424238335, 719885386, 1649760492,
+                                                               596516649, 1189641421, 1025202362, 1350490027,
+                                                               783368690, 1102520059, 2044897763, 1967513926,
+                                                               1365180540, 1540383426, 304089172, 1303455736,
+                                                               35005211, 521595368);
 
     private static Collection<String> toSet(Collection<String> collection) {
         return new HashSet<>(collection);
     }
 
     private final Deque<Integer> stack;
+    private int randomIndex;
 
     public PostFixCalculator() {
         this.stack = new ArrayDeque<>();
+        this.randomIndex = 0;
     }
 
     public void process(String input) {
@@ -67,7 +76,9 @@ public class PostFixCalculator {
     }
 
     private Integer generateRandomNumber() {
-        return 467134654;
+        Integer nextRandom = RANDOMS.get(randomIndex);
+        randomIndex = (randomIndex + 1) % RANDOMS.size();
+        return nextRandom;
     }
 
     private boolean isNumber(String input) {
