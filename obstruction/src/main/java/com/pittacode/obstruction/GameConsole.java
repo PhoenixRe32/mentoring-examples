202b4c0ad1cb8c0a705b9116c239aebc31e10475
Andreas Ανδρέου <andreas.andreou@sky.uk>
2021-06-18 19:22:14 +0300

    obstruction - console - read answer and make it into a tile

--- obstruction/src/main/java/com/pittacode/obstruction/GameConsole.java
+++ obstruction/src/main/java/com/pittacode/obstruction/GameConsole.java
@@ -1,18 +1,29 @@
 package com.pittacode.obstruction;
 
+import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.PrintStream;
 
 public class GameConsole {
 
     private final PrintStream outputStream;
+    private final BufferedReader inputReader;
 
-    public GameConsole(PrintStream outputStream) {
+    public GameConsole(PrintStream outputStream, BufferedReader inputReader) {
         this.outputStream = outputStream;
+        this.inputReader = inputReader;
     }
 
-    public Tile roundQuestion() {
+    public Tile roundQuestion() throws IOException {
         outputStream.print("Make your move, Player> ");
+        String input = inputReader.readLine();
 
-        return null;
+        return convertIntoTile(input);
+    }
+
+    private Tile convertIntoTile(String input) {
+        String[] coordinates = input.split(" +");
+        return new Tile(Integer.parseInt(coordinates[0]),
+                        Integer.parseInt(coordinates[1]));
     }
 }
--- obstruction/src/test/java/com/pittacode/obstruction/GameConsoleTest.java
+++ obstruction/src/test/java/com/pittacode/obstruction/GameConsoleTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test;
 
 import java.io.BufferedReader;
 import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 import java.io.PrintStream;
 import java.io.StringReader;
 
@@ -12,13 +13,27 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class GameConsoleTest {
 
     @Test
-    void shouldAskForTileCoordinatesWhenAskingInput() {
+    void shouldAskForTileCoordinatesWhenAskingInput() throws IOException {
         ByteArrayOutputStream outputStreamCaptor = new ByteArrayOutputStream();
         PrintStream outputStream = new PrintStream(outputStreamCaptor);
-        GameConsole underTest = new GameConsole(outputStream);
+        BufferedReader inputReader = new BufferedReader(new StringReader("0 0"));
+        GameConsole underTest = new GameConsole(outputStream, inputReader);
 
         underTest.roundQuestion();
 
         assertThat(outputStreamCaptor.toString()).isEqualTo("Make your move, Player> ");
+    }
+
+    @Test
+    void shouldReturnTileCoordinatesWhenAskingInput() throws IOException {
+        ByteArrayOutputStream outputStreamCaptor = new ByteArrayOutputStream();
+        PrintStream outputStream = new PrintStream(outputStreamCaptor);
+        BufferedReader inputReader = new BufferedReader(new StringReader("3 4\n"));
+        GameConsole underTest = new GameConsole(outputStream, inputReader);
+
+        Tile tile = underTest.roundQuestion();
+
+        assertThat(tile.x).isEqualTo(3);
+        assertThat(tile.y).isEqualTo(4);
     }
 }
